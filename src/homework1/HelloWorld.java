cccf4072ebbb7ac2d9f23402a245756eb2b34bcd
arranKIM <kimahlan0729@naver.com>
2018-03-13 11:58:14 +0900

    HelloWorld.java

---
+++ src/homework1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String...args) {
+    //print Hello, Wolrd!
+    System.out.println("*");
+        System.out.println("**");
+           System.out.println("***");
+                System.out.println("****");
+                    System.out.println("*****");
+                        System.out.println("******");
+}
+    }
