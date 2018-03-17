34e5c2b7e4cd1a2e732ce2a48ef77f21cdf63712
arranKIM <37091675+arranKIM@users.noreply.github.com>
2018-03-17 22:58:30 +0900

    Update HelloWorld.java

--- src/homework1/HelloWorld.java
+++ src/homework1/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String...args) {
-    //print Hello, Wolrd!
-    System.out.println("Hello, World!");
-        System.out.println("Hello, Java!!");
+      System.out.println("Hello, World!");
+         System.out.println("Hello, Java!!");
 }
     }
