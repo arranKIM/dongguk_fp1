b7fb8aad582c6365a6edb1f0c0a9f1ac3461563a
arranKIM <37091675+arranKIM@users.noreply.github.com>
2018-03-18 19:42:32 +0900

    Add files via upload

---
+++ src/homework1/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public static void main(String...args) {
+        System.out.printf("%d + %d = %d\n", 1, 2, 3);
+        System.out.printf("%d - %d = %d\n", 1, 2, -1);
+        System.out.printf("%d * %d = %d\n", 1, 2, 2);
+    }
+}
